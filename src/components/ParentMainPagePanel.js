8d9ab95aa89bc96941225906ee0abce269d2a13a
dafid1999 <borawskidawid@gmail.com>
2022-05-08 09:55:35 +0200

    Add deposit and withdraw

--- src/components/ParentMainPagePanel.js
+++ src/components/ParentMainPagePanel.js
@@ -5,6 +5,9 @@ import { Button } from "./Button";
 import { faUserPen, faBook} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { LoadingSpinner } from "./LoadingSpinner";
+import {Dialog, DialogActions, DialogContent, DialogTitle} from "@mui/material";
+import {ErrorMessage, Field, Form, Formik} from "formik";
+import * as Yup from "yup";
 
 function ParentMainPagePanel() {
     const BaseUrl = 'https://api.mwis.pl/'
@@ -12,6 +15,17 @@ function ParentMainPagePanel() {
     const [balance, setBalance] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [users, setUsers] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [responseStatus, setResponseStatus] = useState(null);
+
+
+    const handleOpen = () => {
+      setOpen(true);
+    };
+    const handleClose = () => {
+      setOpen(false);
+    };
+
 
     const UserID = async () => {
          const response = await fetch("".concat(`${BaseUrl}`, ['auth/user']),  {
@@ -61,6 +75,39 @@ function ParentMainPagePanel() {
         }
      });
 
+    const Money = Yup.object().shape({
+      amount: Yup
+        .number("Kwota musi być liczbą!")
+        .positive("Kwota musi być większa od 0!")
+        .required("Kwota jest wymagana!")
+    });
+
+    const PositiveMessage = () => (
+        <section className="popUp">
+          <div className="register201">
+            <p>
+              Wpłaciłes pieniadze na konto.
+            </p>
+          </div>
+        </section>
+      );
+
+    const NegativeMessage = () => (
+      <p className="register400">
+        Wystąpił błąd przy próbie wpłaty.
+      </p>
+    );
+
+
+    const displayMessage = () => {
+      if (responseStatus === 201) {
+        return <PositiveMessage />;
+      } else if (responseStatus === 400) {
+        return <NegativeMessage />;
+      }
+    };
+
+
     return (
       <>
         <div className="main">
@@ -70,25 +117,135 @@ function ParentMainPagePanel() {
               {isLoading ? <LoadingSpinner spinnerSize="spin--medium"/> : `${balance}`  }
             </div>
 
-            <Link to="/resetPasswordPanel" className="deposit">
+            <div className="deposit">
               <Button
                 buttonStyle="btn--primary"
                 buttonSize="btn--small"
                 type="submit"
+                onClick={handleOpen}
               >
                 Wpłać pieniądze
               </Button>
-            </Link>
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                >
+                <DialogTitle>Wpłać pieniądze na konto</DialogTitle>
+                <DialogContent>
+                  <Formik
+                    initialValues={{
+                      amount: "",
+                    }}
+                    validationSchema={Money}
+                    onSubmit={async (values) => {
+                      let resStatus = 0;
+                      await fetch("".concat(`${BaseUrl}`, ['deposit/']), {
+                        method: "POST",
+                        headers: {
+                          "Content-Type": "application/json",
+                        },
+                        credentials: "include",
+                        body: JSON.stringify(values, null, 2),
+                      }).then((res) => {
+                        resStatus = res.status;
+                        setResponseStatus(resStatus);
+                      });
+                    }}
+                  >
+                    {({ errors, handleSubmit, touched }) => (
+                    <Form onSubmit={handleSubmit}>
+                      <Field
+                        id="amount"
+                        name="amount"
+                        placeholder="Kwota"
+                        type="text"
+                      />
+                      <p className="errors">
+                        <ErrorMessage name="amount" />
+                      </p>
 
-            <Link to="/resetPasswordPanel" className="withdraw">
+                      <Button
+                        buttonStyle="btn--primary"
+                        buttonSize="btn--large"
+                        type="submit"
+                      >
+                        Wpłać
+                      </Button>
+                    </Form>
+                    )}
+                  </Formik>
+                  {displayMessage()}
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose}>Zamknij</Button>
+                </DialogActions>
+              </Dialog>
+            </div>
+
+            <div className="withdraw">
               <Button
                 buttonStyle="btn--primary"
                 buttonSize="btn--small"
                 type="submit"
+                onClick={handleOpen}
               >
                 Wypłać pieniądze
               </Button>
-            </Link>
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                >
+                <DialogTitle>Wypłać pieniądze z konta</DialogTitle>
+                <DialogContent>
+                  <Formik
+                    initialValues={{
+                      amount: "",
+                    }}
+                    validationSchema={Money}
+                    onSubmit={async (values) => {
+                      let resStatus = 0;
+                      await fetch("".concat(`${BaseUrl}`, ['withdraw/']), {
+                        method: "POST",
+                        headers: {
+                          "Content-Type": "application/json",
+                        },
+                        credentials: "include",
+                        body: JSON.stringify(values, null, 2),
+                      }).then((res) => {
+                        resStatus = res.status;
+                        setResponseStatus(resStatus);
+                      });
+                    }}
+                  >
+                    {({ errors, handleSubmit, touched }) => (
+                    <Form onSubmit={handleSubmit}>
+                      <Field
+                        id="amount"
+                        name="amount"
+                        placeholder="Kwota"
+                        type="text"
+                      />
+                      <p className="errors">
+                        <ErrorMessage name="amount" />
+                      </p>
+
+                      <Button
+                        buttonStyle="btn--primary"
+                        buttonSize="btn--large"
+                        type="submit"
+                      >
+                        Wypłać
+                      </Button>
+                    </Form>
+                    )}
+                  </Formik>
+                  {displayMessage()}
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose}>Zamknij</Button>
+                </DialogActions>
+              </Dialog>
+            </div>
 
             <Link to="/resetPasswordPanel" className="transaction-history">
               <Button
